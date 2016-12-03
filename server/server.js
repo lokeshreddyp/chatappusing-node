94972c8afbf2bccf6f66cd0c31acf5b0c6408fe9
lokeshreddyp <lokeshreddypothapu@gmail.com>
2016-12-03 02:00:10 -0600

    greet new user and alert a msg to client

--- server/server.js
+++ server/server.js
@@ -23,7 +23,18 @@ console.log("hello new user is connected using connect method");
 //    CreatedAt : 123
 //  });
 
+socket.emit('newmessageEvent' , {
+from : 'Admin' ,
+text : 'welcome to chat app',
+  createdAt : new Date().getTime()
+});
 
+
+socket.broadcast.emit('newmessageEvent' , {
+  from : 'Admin' ,
+  text : 'New User has joined',
+  createdAt : new Date().getTime()
+})
 // socket.emit('newmessageEvent', {
 //   From : "lokesh@message.com",
 //   text : "Meet me at 6 pm!!",
@@ -37,11 +48,11 @@ console.log("hello new user is connected using connect method");
 
 socket.on('createmessageEvent' , (listeningcreate_messageevent) => {
 console.log('listeningcreate_messageevent' ,listeningcreate_messageevent);
-//io.emit emits data to all open connections(broadcasting)
+//socket.broadcast.emit emits data to all open connections(broadcasting)
 //when we type socket.emit('createmessageEvent' , {x : 'plr' , y : 'hehe it worked!!'}); then it will be displayed
 //to all the connections even i
 //x and y are valued passed as arguments fromcreatemessageEvent
-io.emit('newmessageEvent' , {
+socket.broadcast.emit('newmessageEvent' , {
   from : listeningcreate_messageevent.x,
   text : listeningcreate_messageevent.y,
    CreatedAt : new Date().getTime()
